50f3a19b11d8a7fb42231fb9e949bebd3687f132
calebpenning <calebpenning@mail.com>
2021-04-29 12:33:34 -0700

    Initial commit of API

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const catsRoutes = require('./routes/cats')
+const ExpressError = require('./expressError')
+
+app.use(express.json())
+app.use("/cats", catsRoutes)
+
+/* 404 Handler */
+
+app.use(function(req, res, next) {
+    return new ExpressError("Not Found", 404)
+})
+
+// General error handler
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+
+    return res.json({
+        error: err.message,
+    })
+})
+
+app.listen(3000, function() {
+    console.log(`Listening on port ${arguments[0]}`)
+})
